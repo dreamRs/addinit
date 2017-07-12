d22f0e646c526ca27b9f36cf8c6b84db3d839346
pvictor <perrier.victor@gmail.com>
2017-07-12 12:26:08 +0200

    add template dashboard

---
+++ inst/www/templates/dashboard/server.R
@@ -0,0 +1,21 @@
+
+
+#  ------------------------------------------------------------------------
+#
+# Title : App - Server
+#    By : {{author}}
+#  Date : {{date}}
+#    
+#  ------------------------------------------------------------------------
+
+
+function(input, output, session) {
+  
+  
+  # callModule(
+  #   module = module_one,
+  #   id = "id_module_one"
+  # )
+  
+  
+}
