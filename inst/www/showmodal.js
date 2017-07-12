d65f688a9b76dfa1bfdf37eae4617c51b2205d71
pvictor <perrier.victor@gmail.com>
2017-07-12 12:24:34 +0200

    clean inst

--- inst/www/showmodal.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  Shiny.addCustomMessageHandler('ShowModal',
-                                function(value) {
-                                  console.log($("#"+value.eltID).modal('show'));
-                                  $("#"+value.eltID).modal('show');
-                                  $('.modal-backdrop').remove();
-                                });
-});
-
